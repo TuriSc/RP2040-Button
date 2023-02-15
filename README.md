4a9c1e86b68f4ca25939ca4c2450897f4e782820
TuriSc <turiscand@gmail.com>
2023-02-15 00:05:04 +0000

    Better example

--- README.md
+++ README.md
@@ -6,22 +6,3 @@ It generates interrupts after listening to GPIO_IRQ events.
 It allows to define multiple buttons simultaneously.
 
 Fork of [jkroso/pico-button.c](https://github.com/jkroso/pico-button.c) including [jkroso/pico-gpio-interrupt.c](https://github.com/jkroso/pico-gpio-interrupt.c), both by Jake Rosoman. MIT license.
-
-## Usage Example
-```c
-#include <button.h>
-#define BUTTON_PIN 21
-
-void onchange(button_t *button_p) {
-  button_t *button = (button_t*)button_p;
-  printf("Button on pin %d changed its state to %d\n", button->pin, button->state);
-}
-
-int main() {
-  stdio_init_all();
-  button_t *my_button = create_button(BUTTON_PIN, onchange);
-  printf("Button created. Its state is %d\n", my_button->state);
-  while (true) tight_loop_contents();
-  return 0;
-}
-```
--- example.c
+++ example.c
@@ -1,17 +1,31 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 #include <button.h>
-#define BUTTON_PIN 21
+#define PLAY_BUTTON 16  // Button on GPIO 16
+#define PAUSE_BUTTON 17  // Button on GPIO 17
 
 void onchange(button_t *button_p) {
   button_t *button = (button_t*)button_p;
   printf("Button on pin %d changed its state to %d\n", button->pin, button->state);
+
+  if(button->state) return; // Ignore button release. Invert the logic if using
+                            // a pullup (internal or external).
+
+  switch(button->pin){
+    case PLAY_BUTTON:
+        printf("Play\n");
+    break;
+    case PAUSE_BUTTON:
+        printf("Pause\n");
+    break;
+  }
 }
 
 int main() {
   stdio_init_all();
-  button_t *my_button = create_button(BUTTON_PIN, onchange);
-  printf("Button created. Its state is %d\n", my_button->state);
+  button_t *play_button = create_button(PLAY_BUTTON, onchange);
+  button_t *pause_button = create_button(PAUSE_BUTTON, onchange);
+
   while (true) tight_loop_contents();
   return 0;
 }
